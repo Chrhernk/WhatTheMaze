f952e6c587adddf936b5a683cb3cd06bdf588a42
CHRISTOPHER HERNKE <CHRHERNK@uat.edu>
2021-04-03 18:55:33 -0500

    Vert works

--- maze/Main.cpp
+++ maze/Main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <time.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <windows.h>
 
 void MazeGeneration(int width, int height);
 void PrintMaze(int width, int height);
 void WallDivision(int x, int y, int width, int height);
+void loadingBar();
 
 const int WALL = 1;
 const int SPACE = 0;
@@ -19,6 +22,8 @@ int main()
 	int width = 0;
 	int height = 0;
 	int Selection = 0;
+	loadingBar();
+
 	std::cout << "\n\n\t *** Maze Generation *** \n";
 
 	std::cout << "Please Select What size Maze you wish to generate...\n\n";
@@ -83,35 +88,88 @@ void PrintMaze(int width, int height)
 
 void WallDivision(int x, int y, int width, int height)
 {
-	bool HOR = true;
-	if (width < 2 || height <2)
-	{
-		return;
-	}
-	if (width > height)
-	{
-		HOR = false;
-	}
-	if (HOR)
-	{
-		int Startx = x;
-		int Starty = y + height/2;
 
-		int NewW = width;
-		int NewH = height / 2; 
-		
-		for (int i = 0; i < NewW; i++)
+		loadingBar();
+		bool HOR = false;
+		if (width < 2 || height < 2)
 		{
-			Maze[Startx + i][Starty] = WALL;
+			return;
 		}
+		if (width > height)
+		{
+			HOR = false;
+		}
+		if (HOR)
+		{
+			int Startx = x;
+			int Starty = y + height / 2;
 
-		int Holex = Startx + rand() % width;
-		int Holey = Starty;
-		
-		Maze[Holex][Holey] = SPACE; 
+			int NewW = width;
+			int NewH = height / 2;
 
+			for (int i = 0; i < NewW; i++)
+			{
+				Maze[Startx + i][Starty] = WALL;
+			}
+
+			int Holex = Startx + rand() % width;
+			int Holey = Starty;
+
+			Maze[Holex][Holey] = SPACE;
+
+		}
+		else
+		{
+			int Startx = x + width / 2;
+			int Starty = y;
+
+			int NewW = width / 2;
+			int NewH = height;
+
+			for (int i = 0; i < NewH; i++)
+			{
+				Maze[Startx][Starty + i] = WALL;
+			}
+
+			int Holex = Startx;
+			int Holey = Starty + rand() % height;
+
+			Maze[Holex][Holey] = SPACE;
+
+		}
+	
+
+}
+
+void loadingBar()
+{
+	// 0 - black background,
+	// A - Green Foreground
+	system("color 0A");
+
+	// Initialize char for printing
+	// loading bar
+	char a = 177, b = 219;
+
+	printf("\n\n\n\n");
+	printf("\n\n\n\n\t\t\t\t\t" "Loading...\n\n");
+	printf("\t\t\t\t\t");
+
+	// Print initial loading bar
+	for (int i = 0; i < 26; i++)
+		printf("%c", a);
+
+	// Set the cursor again starting
+	// point of loading bar
+	printf("\r");
+	printf("\t\t\t\t\t");
+
+	// Print loading bar progress
+	for (int i = 0; i < 26; i++) {
+		printf("%c", b);
+
+		// Sleep 
+		Sleep(10);
 	}
-	else   \
-
-
+	printf("\n\n\n");
 }
